d50d5efbaa28e8df6c6277634a96a3ee709af476
Everton19 <evertontp5@gmail.com>
2025-02-23 15:38:38 -0300

    criação do componente de card para a categoria e utilização de props

---
+++ src/components/CardCategory.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import type ICategory from '@/interfaces/ICategory';
+import type { PropType } from 'vue';
+
+export default {
+    props: {
+        category: {
+            type: Object as PropType<ICategory>,
+            required: true
+        }
+    }
+}
+
+</script>
+
+<template>
+    {{ category.nome }}
+</template>
